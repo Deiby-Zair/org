a30b50a572c594a49e8bfbca149278ac6f8cced4
Deiby-Zair <deybyzm@unicauca.edu.co>
2023-06-01 22:33:56 -0500

    Estructura para mostrar equipos

--- src/components/ListaOpciones/index.js
+++ src/components/ListaOpciones/index.js
@@ -5,16 +5,6 @@ const ListaOpciones = (props) => {
     //  return <option></option>
     // })
 
-    const equipos = [
-        "Programación",
-        "Front End",
-        "Data Science",
-        "Devops",
-        "UX y Diseño",
-        "Móvil",
-        "Innovación y Gestión"
-    ]
-
     const manejarCambio = (e) => {
         console.log("cambio", e.target.value);
         props.actualizarEquipo(e.target.value);
@@ -23,9 +13,11 @@ const ListaOpciones = (props) => {
     return <div className="lista-opciones">
         <label>Equipos</label>
         <select value={props.valor} onChange={manejarCambio}>
-            <option value="" disabled defaultValue="" hidden>Seleccionr equipo</option>
-            {equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo}</option>
-            )}
+            <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
+            {
+                props.equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo}</option>
+                )
+            }
         </select>
     </div>
 }
